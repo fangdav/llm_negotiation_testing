77d6f1e07f193bf0a10206202c05e04de1075826
Denis Anisimov <dbanisimov@gmail.com>
2023-06-14 19:50:28 +0300

    Merge Edited_UI_Version2.0 into main

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,13 +2,13 @@ import { EmpiricaClassic } from "@empirica/core/player/classic";
 import { EmpiricaContext } from "@empirica/core/player/classic/react";
 import { EmpiricaMenu, EmpiricaParticipant } from "@empirica/core/player/react";
 import React from "react";
-import "virtual:windi.css";
 import { Game } from "./Game";
 import { SubjectiveValueSurvey } from "./intro-exit/SubjectiveValueSurvey";
 import { Consent } from "./intro-exit/Consent";
 import { Result } from "./intro-exit/Result";
 import { PartnerRatingSurvey } from "./intro-exit/PartnerRating";
 import { HumannessQuestion } from "./intro-exit/HumannessQuestion";
+import { Demographic } from "./intro-exit/Demographic";
 
 export default function App() {
   const urlParams = new URLSearchParams(window.location.search);
@@ -22,12 +22,18 @@ export default function App() {
   }
 
   function exitSteps({ game, player }) {
-    return [Result, SubjectiveValueSurvey, PartnerRatingSurvey, HumannessQuestion];
+    return [
+      Result,
+      SubjectiveValueSurvey,
+      PartnerRatingSurvey,
+      HumannessQuestion,
+      Demographic,
+    ];
   }
 
   return (
     <EmpiricaParticipant url={url} ns={playerKey} modeFunc={EmpiricaClassic}>
-      <div className="h-screen relative">
+      <div className="relative h-screen">
         <EmpiricaMenu />
         <div className="h-full overflow-auto">
           <EmpiricaContext
--- client/src/useGameMechanics.jsx
+++ client/src/useGameMechanics.jsx
@@ -16,13 +16,14 @@ import { useCallback, useEffect, useMemo, useRef } from "react";
  *   id: string;
  *   type: MessageType;
  *   text: string;
+ *   originalText?: string;
  *   playerId: string;
  *   gamePhase: string;
  *   timestamp: number;
  *   noDealStatus?: "pending" | "ended" | "continued" | "unilateral";
  *   proposal?: number;
  *   proposalStatus?: "pending" | "rejected" | "accepted";
- *   agentType: "user" | "agent";
+ *   agentType: "user" | "assistant";
  * }} Message
  */
 
