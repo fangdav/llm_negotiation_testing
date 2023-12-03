0687ed94410713ff38d0540258bf6577eab6e149
Denis Anisimov <denis@introwise.com>
2023-12-03 08:29:08 -0800

    Remove browser confirmation dialogue

--- client/src/ChatCommon.jsx
+++ client/src/ChatCommon.jsx
@@ -132,15 +132,6 @@ export function ChatCommon({
   };
 
   const onNewNoDeal = async () => {
-    if (unilateralNoDeal) {
-      const res = window.confirm(
-        `Are you sure you want to walk away without a deal?`
-      );
-      if (!res) {
-        return;
-      }
-    }
-
     setBusy(true);
 
     try {
